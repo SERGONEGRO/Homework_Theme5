660e6f0246da1cda2d1275935eda2459996eac67
Сергей Воробьев <kazan1003@gmail.com>
2021-09-30 12:16:27 +0300

    task 2,3 completed

---
+++ Homework_Theme5_task3.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Задание 3.Создание метода, принимающего строку и возвращающего новую строку, в которой удалены все рядом стоящие повторяющиеся символы
+
+//Что нужно сделать
+//Создайте метод, который принимает произвольный текст в виде строки.
+//Этот метод возвращает новую строку, в которой отсутствуют все повторяющиеся, рядом стоящие символы.
+
+//Примеры:
+
+//ПППОООГГГООООДДДААА >>> ПОГОДА
+
+//Ххххоооорррооошшшиий деееннннь >>> хороший день
+
+//Регистр символов не имеет значения.
+
+
+namespace Homework_Theme5_task3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку : ");
+
+            string stringWithDoubles = Console.ReadLine();
+
+            Console.WriteLine($"Преобразованная строка: {ReturnStringWithoutDoubles(stringWithDoubles)}");
+
+        }
+
+        /// <summary>
+        /// Метод, возвращающий строку без повторяющихся букв
+        /// </summary>
+        /// <param name="s">Входящая строка</param>
+        /// <returns></returns>
+        static string ReturnStringWithoutDoubles(string s)
+        {
+            //преобразуем строку в List из символов
+            List<char> chars = s./*ToLower().*/ToCharArray().ToList();
+
+            //Если текущий символ=предыдущему, то удаляем предыдущий , уменьшаем счетчик на 1
+            for (int i = 1;i<chars.Count; i++)
+            {
+                if (chars[i-1] == chars[i]) { chars.RemoveAt(i-1); i--; }
+            }
+            //foreach (var e in chars) Console.WriteLine(e);
+
+            //Преобразуем List букв в строку, для этого вначале преобразуем его в массив букв.
+            string result = new string(chars.ToArray());
+            return result;
+        }
+    }
+}
--- Homework_theme5_task2.1/Program.cs
+++ Homework_theme5_task2.1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //Первый метод принимает строку с текстом и возвращает строку, содержащую слово с минимальной длиной.
+//Второй метод также принимает на вход текст, но возвращает одно или несколько слов с максимальной длиной.
 
 namespace Homework_theme5_task2._1
 {
@@ -15,22 +16,58 @@ namespace Homework_theme5_task2._1
             Console.WriteLine("Введите строку : ");
             string str = Console.ReadLine();
 
-            Console.WriteLine($"Минимальное слово : {ReturnMinWord(str)}");
+            Console.WriteLine($"Минимальное по длине слово (слова) : ");
+
+            string[] resultMin = ReturnMinWord(str);
+            foreach (var e in resultMin) Console.WriteLine(e);
+
+            Console.ReadKey();
+
+            Console.WriteLine($"Максимальное по длине слово (слова) : ");
+
+            string[] resultMax = ReturnMaxWord(str);
+            foreach (var e in resultMax) Console.WriteLine(e);
+
+
 
         }
 
-        static string ReturnMinWord(string s)
+        /// <summary>
+        /// Метод, принимающий строку и возвращающий массив минимальных по длине строк
+        /// </summary>
+        /// <param name="s">Входная строка</param>
+        /// <returns></returns>
+        static string[] ReturnMinWord(string s)
         {
-            
-            string[] result;
-            result = s.Split(charSeparators, StringSplitOptions.None);
-            string minWord = result[0];
-            //foreach (var e in result) Console.WriteLine(e);
-            for (int i=0; i < result.Length; i++)
-            {
-               if (minWord.Length > result[i].Length) minWord = result[i] ;
-            }
-            return minWord;
+            //разделяем строку на массив слов
+            string[] result = s.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            //ищем длину минимального слова
+            int minLenght = (from n in result select n.Length).Min();
+
+            //заполняем массив всеми словами, чья длина = минимальной
+            string[] minWords = Array.FindAll(result, x => x.Length == minLenght);
+
+            return minWords;
+        }
+
+        /// <summary>
+        /// Метод, принимающий строку и возвращающий массив максимальных по длине строк
+        /// </summary>
+        /// <param name="s">Входная строка</param>
+        /// <returns></returns>
+        static string[] ReturnMaxWord(string s)
+        {
+            //разделяем строку на массив слов
+            string[] result = s.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            //ищем длину максимального слова
+            int maxLenght = (from n in result select n.Length).Max();
+
+            //заполняем массив всеми словами, чья длина = максимальной
+            string[] minWords = Array.FindAll(result, x => x.Length == maxLenght);
+
+            return minWords;
         }
     }
 }
